9f0b3100f957920cab12200167c7d77fac5d36b2
Alexey Shadow <alexey.bakushev@gemforge.ru>
2024-12-30 02:39:26 +0300

    icons upload testing

--- src/components/ProblemBoard/ProblemBoardTabs/ProblemBoardTabs.jsx
+++ src/components/ProblemBoard/ProblemBoardTabs/ProblemBoardTabs.jsx
@@ -4,7 +4,7 @@ export const ProblemBoardTabs = () => {
   return (
     <div className={styles.container}>
       <img
-        src="C:\Work\code-editor\src\assets\description.png"
+        src="/src/assets/description.png"
         className={styles.description_icon}
       />
       <span>Description</span>
